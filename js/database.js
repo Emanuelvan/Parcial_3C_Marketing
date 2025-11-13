fe0b37ba5380d8b8ab61710b33855f13faebe7e0
Emanuelvan <emanuelvan00@gmail.com>
2025-11-12 23:36:48 -0500

    Cambio dirección imagenes

--- js/database.js
+++ js/database.js
@@ -3,10 +3,10 @@
  */
 
 const DB_PRODUCTOS = [
-    { id: 1, nombre: 'Hoodie "Black Street"', categoria: 'Hoodies', precio: 120000, img: './Imagenes/Hoodie _Black_Street.png' },
-    { id: 2, nombre: 'Hoodie "Retro Gray"', categoria: 'Hoodies', precio: 115000, img: './Imagenes/Hoodie_Retro_Gray.png' },
-    { id: 3, nombre: 'Gorra "NYC Flat"', categoria: 'Gorras', precio: 75000, img: './Imagenes/Gorra_NYC_Flat.png' },
-    { id: 4, nombre: 'Gorra "Classic White"', categoria: 'Gorras', precio: 70000, img: './Imagenes/Gorra_Classic_White.png' },
-    { id: 5, nombre: 'Buso Oversize "Storm"', categoria: 'Busos oversize', precio: 95000, img: './Imagenes/Buso_Oversize_Storm.png' },
-    { id: 6, nombre: 'Buso Oversize "Skyline"', categoria: 'Busos oversize', precio: 99000, img: './Imagenes/Buso_Oversize_Skyline.png' }
+    { id: 1, nombre: 'Hoodie "Black Street"', categoria: 'Hoodies', precio: 120000, img: 'https://github.com/Emanuelvan/Parcial_3C_Marketing/blob/main/Imagenes/Hoodie%20_Black_Street.png?raw=true' },
+    { id: 2, nombre: 'Hoodie "Retro Gray"', categoria: 'Hoodies', precio: 115000, img: 'https://github.com/Emanuelvan/Parcial_3C_Marketing/blob/main/Imagenes/Hoodie_Retro_Gray.png?raw=true' },
+    { id: 3, nombre: 'Gorra "NYC Flat"', categoria: 'Gorras', precio: 75000, img: 'https://github.com/Emanuelvan/Parcial_3C_Marketing/blob/main/Imagenes/Gorra_NYC_Flat.png?raw=true' },
+    { id: 4, nombre: 'Gorra "Classic White"', categoria: 'Gorras', precio: 70000, img: 'https://github.com/Emanuelvan/Parcial_3C_Marketing/blob/main/Imagenes/Gorra_Classic_White.png?raw=true' },
+    { id: 5, nombre: 'Buso Oversize "Storm"', categoria: 'Busos oversize', precio: 95000, img: 'https://github.com/Emanuelvan/Parcial_3C_Marketing/blob/main/Imagenes/Buso_Oversize_Storm.png?raw=true' },
+    { id: 6, nombre: 'Buso Oversize "Skyline"', categoria: 'Busos oversize', precio: 99000, img: 'https://github.com/Emanuelvan/Parcial_3C_Marketing/blob/main/Imagenes/Buso_Oversize_Skyline.png?raw=true' }
 ];
